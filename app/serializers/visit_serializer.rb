82a6e76c9403b23c4d14e93196ab126a096cae62
kirirotha <kirirotha@gmail.com>
2020-10-15 00:20:55 -0500

    improved CRUD function

--- app/serializers/visit_serializer.rb
+++ app/serializers/visit_serializer.rb
@@ -1,4 +1,4 @@
 class VisitSerializer < ActiveModel::Serializer
-  attributes :id, :location_name, :latitude, :longitude, :date_visited, :description, :phone, :reservable, :email
+  attributes :id, :trip_id, :location_name, :latitude, :longitude, :date_visited, :description, :phone, :reservable, :email
   has_one :trip
 end
